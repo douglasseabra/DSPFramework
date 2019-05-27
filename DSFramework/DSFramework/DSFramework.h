49d63a4c7ff465bc3ca81d8de477aa1f05bfaa74
Douglas Seabra <douglas.pedrosa@hotmart.com>
2019-05-27 15:11:30 -0300

    Finish initial

---
+++ DSFramework/DSFramework/DSFramework.h
@@ -0,0 +1,19 @@
+//
+//  DSFramework.h
+//  DSFramework
+//
+//  Created by Douglas Seabra on 27/05/19.
+//  Copyright © 2019 Douglas Seabra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DSFramework.
+FOUNDATION_EXPORT double DSFrameworkVersionNumber;
+
+//! Project version string for DSFramework.
+FOUNDATION_EXPORT const unsigned char DSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSFramework/PublicHeader.h>
+
+
